1993ca61c917a72ab62be2f27c2f5b9afec6904b
Clawyer <valentin.deal@gmail.com>
2022-07-28 10:04:47 +0200

    add google provider

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Ip, Post, Req } from "@nestjs/common";
+import { Body, Controller, Delete, HttpException, HttpStatus, Ip, Post, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import RefreshTokenDto from "./dto/refresh-token.dto";
 import { LoginDto } from "./dto/login.dto";
+import GoogleTokenDto from "./dto/google-token.dto";
 
 @Controller("auth")
 export class AuthController {
@@ -13,6 +14,24 @@ export class AuthController {
       ipAddress: ip,
       userAgent: request.headers["user-agent"],
     });
+  }
+  @Post("/google/login")
+  async googleLogin(@Body() body: GoogleTokenDto, @Req() req, @Ip() ip: string): Promise<{ accessToken: string; refreshToken: string }> {
+    const result = await this.authService.loginGoogleUser(body.token, {
+      userAgent: req.headers["user-agent"],
+      ipAddress: ip,
+    });
+    if (result) {
+      return result;
+    } else {
+      throw new HttpException(
+        {
+          status: HttpStatus.UNAUTHORIZED,
+          error: "Error while logging in with google",
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
   }
 
   @Post("refresh")
